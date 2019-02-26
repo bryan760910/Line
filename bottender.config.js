54e24f0e0ecaf0e97d7dfc102b7432b2465e7031
bryan <bryan099767@gmail.com>
2019-02-26 14:13:16 +0800

    first push

---
+++ bottender.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  line: {
+    accessToken: 'IZnyHeiFN5NM6nPHSZaGgfPobclQ+0qkLzK+6rDsYcLCUoPMWPjpF1FjHTbAuqzJzA0CPvjAMaCV9/rjYHd9/AQmAT8qS7q01AMTwAg0F77wgqqsm9qE/wfKtMDeeBt9n+htNwWft4sUMUPMEp9f4QdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '7b2f19d682d1253c00b3b1053dca0245'
+  }
+};
