26ec8c7046cc1de93b59e278fb855311365c46ab
bryan <bryan099767@gmail.com>
2019-02-26 14:22:35 +0800

    FIXED BUG

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ bot.onEvent(async context => {
 
 const server = createServer(bot);
 
-server.listen((process.env.PORT || 5000, () => {
+server.listen((process.env.PORT || 5000), () => {
   console.log('server is running on 5000 port...');
 });
