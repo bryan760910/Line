1ab3488b9d8449e0040fdbf45fe2d8df5144e745
bryan <bryan099767@gmail.com>
2019-02-26 15:17:38 +0800

    更換TOKEN

--- bottender.config.js
+++ bottender.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   line: {
-    accessToken: 'IZnyHeiFN5NM6nPHSZaGgfPobclQ+0qkLzK+6rDsYcLCUoPMWPjpF1FjHTbAuqzJzA0CPvjAMaCV9/rjYHd9/AQmAT8qS7q01AMTwAg0F77wgqqsm9qE/wfKtMDeeBt9n+htNwWft4sUMUPMEp9f4QdB04t89/1O/w1cDnyilFU=',
+    accessToken: '3ZM2tJJu05urKKsOsRpivFjFixgWlJulgMHfXE5UuZdh2VowVG9W7MxcyLaCwa9azA0CPvjAMaCV9/rjYHd9/AQmAT8qS7q01AMTwAg0F77ZhxMeij+ooXPpVa/pKD1bH6GrT6ww7MkG3L1If1P+FwdB04t89/1O/w1cDnyilFU=',
     channelSecret: '7b2f19d682d1253c00b3b1053dca0245'
   }
 };
