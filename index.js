bd021d107e341d4f63e898e518f0982d28d2e242
bryan <bryan099767@gmail.com>
2019-02-27 11:52:40 +0800

    加入股票資訊

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const { LineBot } = require('bottender');
 const { createServer } = require('bottender/express');
-
 const config = require('./bottender.config.js').line;
+const stock = require('././stock/stock.js');
+
 
 const bot = new LineBot({
-  accessToken: config.accessToken,
-  channelSecret: config.channelSecret,
-  sendMethod: 'reply', // Default: 'push'
+    accessToken: config.accessToken,
+    channelSecret: config.channelSecret,
+    sendMethod: 'reply', // Default: 'push'
 });
 
 bot.onEvent(async context => {
-  console.log(context)
-  await context.sendText('我是LINE機器人阿!!!!! 試試看 !!!');
+
+    try {
+		let stockInfoDict = await stock.stockInfo(context.event.message.text);
+		let stockInfoMsg = stock.stockInfoMsg(context.event.message.text, stockInfoDict);
+		await context.sendText(stockInfoMsg);
+	} catch (error) {
+		let stockInfoMsg = stock.stockInfoMsg(context.event.message.text, error);
+		await context.sendText(stockInfoMsg);
+	}
+    // console.log(context)
+    // await context.sendText('我是LINE機器人阿!!!!! 試試看 !!!');
 });
-
-
-
 
 const server = createServer(bot);
 
 server.listen((process.env.PORT || 5000), () => {
-  console.log('server is running on 5000 port...');
+    console.log('server is running on 5000 port...');
 });
