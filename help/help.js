2e6ac5a66e8ca2107cdc535390ad8c404a4ae1d3
Mona Lisa <bryan.cheng@sinopac.com>
2021-08-19 15:47:51 +0800

    更新

--- help/help.js
+++ help/help.js
@@ -5,6 +5,7 @@ help.getlist = () => {
     return [
         { category : "help", desc : "ask help", descCHZN: "語法說明"},
         { category : "hero", desc : "ask hero 英靈名稱", descCHZN: "查詢英靈"},
+        { category : "hero", desc : "ask hero all", descCHZN: "可查詢的英靈"},
     ];
 };
 
