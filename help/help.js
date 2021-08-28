5e13d66ca10e125e481ee4420a985f03a68d5868
bryan <s9421051@gmail.com>
2021-08-28 13:04:08 +0800

    更新 HELP

--- help/help.js
+++ help/help.js
@@ -4,9 +4,9 @@ const help = {};
 help.getlist = () => {
     return [
         { category : "help", desc : "ask help", descCHZN: "語法說明"},
-        { category : "hero", desc : "ask hero 英靈名稱", descCHZN: "查詢英靈"},
-        { category : "hero", desc : "ask hero all", descCHZN: "可查詢的英靈"},
+        { category : "hero", desc : "ask hero [all | 英靈名稱] ", descCHZN: "[可查詢的英靈 | 查詢英靈]"},
         { category : "ado", desc : "ask ado [hand | waist | head | body]", descCHZN: "可查詢全部飾品"},
+        { category : "dream", desc : "ask dream [all | ]", descCHZN: "[可查詢的入夢符 | 查詢入夢符]"},
     ];
 };
 
